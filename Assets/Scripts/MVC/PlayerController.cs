d5fd385dbcf7af01cf186bb83c27308de667879b
gxvelxx <wolf2005k@naver.com>
2025-11-11 16:22:05 +0900

    [feat] Player Jump
    플레이어 점프 추가

--- Assets/Scripts/MVC/PlayerController.cs
+++ Assets/Scripts/MVC/PlayerController.cs
@@ -6,11 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerView playerView; // UI 패널이 어딘가 있다고 가정
     PlayerModel playerModel; // 플레이어이름 등등
-    InputAction damageAction;
 
+    InputAction damageAction;
     InputAction moveAction;
+    InputAction jumpAction;
+
     Vector3 moveDirection;
     private float moveSpeed = 10f;
+    private float jumpSpeed = 10f;
+
+    Rigidbody rb;
+    private bool isGround = true;
 
     private void Awake()
     {
@@ -22,8 +28,8 @@ public class PlayerController : MonoBehaviour
         }
 
         damageAction = InputSystem.actions["Attack"];
-
         moveAction = InputSystem.actions["Move"];
+        jumpAction = InputSystem.actions["Jump"];
     }
 
     private void Start()
@@ -31,16 +37,21 @@ public class PlayerController : MonoBehaviour
         playerModel.OnHealthChange += playerView.UpdateHealthUI; // 여기가 핵심코드! 연결을 담당하는 컨트롤러가 분리되었음
         playerView.UpdateHealthUI(playerModel.Health);
 
+        rb = GetComponent<Rigidbody>();
+
+        //이동
         moveAction.performed += (ctx) =>
         {
             Vector2 direction = ctx.ReadValue<Vector2>();
             moveDirection = new Vector3(direction.x, 0, direction.y);
         };
-
         moveAction.canceled += (ctx) =>
         {
             moveDirection = Vector3.zero;
         };
+
+        //점프
+        jumpAction.performed += (ctx) => OnJump();
     }
 
     private void Update()
@@ -55,14 +66,33 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         damageAction.performed += OnDamageTriggered;
+        jumpAction.Enable();
     }
     private void OnDisable()
     {
         damageAction.performed -= OnDamageTriggered;
+        jumpAction.Disable();
     }
 
     void OnDamageTriggered(InputAction.CallbackContext ctx)
     {
         playerModel.TakeDamage(10);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts.Length > 0 && collision.contacts[0].normal.y > 0.5f)
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnJump()
+    {
+        if (isGround)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpSpeed, rb.linearVelocity.z);
+            isGround = false;
+        }    
     }
 }
